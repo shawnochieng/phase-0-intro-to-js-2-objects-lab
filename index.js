47602f3aca4340769ff179b0f44a1633f283e461
duthu <duthujoe05@gmail.com>
2025-09-24 14:31:15 +0300

    Init: Add functional index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee= {name:'Shawn', streetaddress:'12 Mombasa Street'}
+
+// Function to non-destructively update employee with a new key-value pair
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+      ...employee,
+      [key]: value
+    };
+  }
+  
+
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] =value;
+    return employee;
+  }
+  
+  function deleteFromEmployeeByKey(employee, key){
+    const newEmployee ={...employee};
+    delete newEmployee[key];
+    return newEmployee
+  }
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee
+}
